c0c049397dd0c8314c7397ac7ef37912310a81c5
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-03-07 00:18:35 -0300

    Login component added

---
+++ src/components/Login/index.js
@@ -0,0 +1,136 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { login } from '../../actions/usersAction'
+
+import {
+    Grid,
+    withStyles,
+    Card,
+    CardHeader,
+    CardContent,
+    TextField,
+    Button,
+    CircularProgress
+} from '@material-ui/core'
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        minHeight: '100vh'
+    },
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    textField: {
+        width: '90%',
+    },
+    loginBtn: {
+        marginTop: '32px',
+        marginBottom: '8px',
+        width: '90%'
+    },
+    error: {
+        minHeight: '18px',
+        color: '#F00',
+        textAlign: 'center'
+    }
+})
+
+class Login extends Component {
+
+    state = {
+        username: {
+            error: false,
+            value: ' '
+        },
+        password: {
+            error: false,
+            value: ''
+        },
+        loading: false
+    }
+
+    handleInputChange = key => event => {
+        let value = event.target.value
+        this.setState((state) => ({
+            [key]: {
+                ...state[key],
+                value
+            }
+        }))
+    }
+
+    handleSubmit = event => {
+
+        event.preventDefault()
+        this.setState({ loading: true })
+        const username = this.state.username.value
+        const password = this.state.password.value
+        const user = { username, password }
+
+        this.props.login(user)
+            .then(() => { this.props.history.push('/') })
+
+        setTimeout(() => { this.setState({ loading: false }) }, 1000)
+
+    }
+
+    render() {
+        const { classes, user } = this.props
+        return (
+            <Grid container alignItems="center" justify="center" className={classes.root}>
+                <Grid item xs={10} sm={6} lg={4} >
+                    <Card>
+                        <CardHeader title="Login Form" />
+                        <CardContent>
+                            <div className={classes.error}>{user.loggingError ? user.error.detail : ' '}</div>
+                            < form noValidate autoComplete="off" className={classes.container} onSubmit={this.handleSubmit}>
+                                <TextField
+                                    id="username"
+                                    label="Username"
+                                    className={classes.textField}
+                                    value={this.state.username.value}
+                                    margin="normal"
+                                    onChange={this.handleInputChange('username')}
+                                />
+                                <TextField
+                                    id="password"
+                                    label="Password"
+                                    type="password"
+                                    className={classes.textField}
+                                    margin="normal"
+                                    value={this.state.password.value}
+                                    onChange={this.handleInputChange('password')}
+                                />
+                                <Button variant="contained" color="primary" type="submit" className={classes.loginBtn}>
+                                    {!this.state.loading ? 'Login'
+                                        : <CircularProgress color="inherit" size={24} />
+                                    }
+                                </Button>
+                            </form>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+const mapStateToProps = ({ user }) => {
+    return {
+        user
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        login: (user) => dispatch(login(user))
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(withStyles(styles)(Login))
