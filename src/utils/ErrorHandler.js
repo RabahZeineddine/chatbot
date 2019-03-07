be590a1a09cae097ba0d49dc2015d056f3c8b454
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-03-07 00:15:52 -0300

    Error handling file added

---
+++ src/utils/ErrorHandler.js
@@ -0,0 +1,34 @@
+import { errorDetail } from '../config'
+
+export default class ErrorHandler {
+    constructor(error = {}) {
+        if (typeof error == 'number') {
+            this.setErrorByStatus(error)
+        } else {
+            this.setError(error)
+        }
+
+    }
+
+    setError(error) {
+        console.log(error)
+        this.error = true
+        this.status = error.status || ''
+        this.detail = error.detail || 'An error occurred, try again later.'
+
+    }
+
+    format() {
+        return {
+            error: this.error,
+            status: this.status,
+            detail: this.detail
+        }
+    }
+
+    setErrorByStatus(status) {
+        this.error = true
+        this.status = status || ''
+        this.detail = errorDetail(status)
+    }
+}
