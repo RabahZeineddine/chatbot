3de0613140c199ca20973dab769a128d872e6a26
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-03-07 00:20:26 -0300

    basic chat bot component added

---
+++ src/components/Chatbot/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Chatbot extends Component {
+
+    render() {
+        return (
+            <div>Chat bot page</div>
+        )
+    }
+}
+
+
+export default Chatbot
