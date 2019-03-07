21f01648a765c410df0df8578474cdfb5e0b054d
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-03-07 00:19:04 -0300

    Private route added

---
+++ src/components/PrivateRoute/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+function PrivateRoute({ component: Component, ...rest }) {
+
+
+    return <Route
+        {...rest}
+        render={props =>
+            rest.user.isLogged ? (
+                <Component {...props} />
+            )
+                : (
+                    <Redirect
+                        to={{
+                            pathname: "/login",
+                            state: { from: props.location }
+                        }}
+                    />
+                )
+        }
+    />
+}
+
+const mapStateToProps = ({ user }) => {
+    return {
+        user
+    }
+}
+
+
+export default connect(mapStateToProps)(PrivateRoute)
